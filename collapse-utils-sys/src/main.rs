eaf0671b4e8bd4324a3c248a3860948012a5dd56
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-04-01 21:21:37 +0100

    feat: consolidate some utils into the "sys" util

--- collapse-utils-sys/src/main.rs
+++ collapse-utils-sys/src/main.rs
@@ -1,3 +1,46 @@
+use std::env::{
+    args,
+    consts::{
+        ARCH,
+        OS,
+        FAMILY,
+        EXE_EXTENSION,
+        EXE_SUFFIX,
+        DLL_EXTENSION,
+        DLL_SUFFIX,
+        DLL_PREFIX
+    }
+};
+
 fn main() {
-    println!("{}", std::env::consts::FAMILY);
+    let args: Vec<_> = args().collect();
+    match args[1].as_str() {
+        "-arch" => {
+            println!("{}", ARCH)
+        }
+        "-os" => {
+            println!("{}", OS)
+        }
+        "-family" => {
+            println!("{}", FAMILY)
+        }
+        "-ext" => {
+            println!("{}", EXE_EXTENSION)
+        }
+        "-exs" => {
+            println!("{}", EXE_SUFFIX)
+        }
+        "-libt" => {
+            println!("{}", DLL_EXTENSION)
+        }
+        "-libs" => {
+            println!("{}", DLL_SUFFIX)
+        }
+        "-libp" => {
+            println!("{}", DLL_PREFIX)
+        }
+        &_ => {
+            //
+        }
+    }
 }
