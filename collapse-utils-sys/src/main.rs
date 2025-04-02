4a9c4d924a63e1c23cb7237885252fbd2fd88b38
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-04-03 00:35:50 +0100

    refactor: move "sys" util to a clap-rs based system

--- collapse-utils-sys/src/main.rs
+++ collapse-utils-sys/src/main.rs
@@ -1,55 +1,114 @@
-use std::env::{
-    args,
-    consts::{ARCH, DLL_EXTENSION, DLL_PREFIX, DLL_SUFFIX, EXE_EXTENSION, EXE_SUFFIX, FAMILY, OS},
+use clap::{Arg, ArgAction, command};
+use std::env::consts::{
+    ARCH, DLL_EXTENSION, DLL_PREFIX, DLL_SUFFIX, EXE_EXTENSION, EXE_SUFFIX, FAMILY, OS,
 };
 
 fn main() {
-    let args: Vec<_> = args().collect();
-    for arg in args {
-        match arg.as_str() {
-            "-a" | "--arch" => {
-                println!("{}", ARCH)
-            }
-            "-o" | "--operating-system" => {
-                println!("{}", OS)
-            }
-            "-f" | "--family" => {
-                println!("{}", FAMILY)
-            }
-            "-ext" | "--ex-extension" => {
-                println!("{}", EXE_EXTENSION)
-            }
-            "-exs" | "--ex-suffix" => {
-                println!("{}", EXE_SUFFIX)
-            }
-            "-libt" | "--lib-extension" => {
-                println!("{}", DLL_EXTENSION)
-            }
-            "-libs" | "--lib-suffix" => {
-                println!("{}", DLL_SUFFIX)
-            }
-            "-libp" | "--lib-prefix" => {
-                println!("{}", DLL_PREFIX)
-            }
-            "-h" | "--help" => {
-                help();
-            }
-            &_ => {
-                //
-            }
+    let matches = command!()
+        .about("Shows information about the system.")
+        .arg(
+            Arg::new("arch")
+                .short('a')
+                .long("architecture")
+                .help("Print the CPU architecture")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("os")
+                .short('o')
+                .long("operating-system")
+                .help("Print the operating system")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("family")
+                .short('f')
+                .long("family")
+                .help("Print the OS family")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("ex-extension")
+                .short('e')
+                .long("ex-extension")
+                .help("Print the executable extension")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("ex-suffix")
+                .short('E')
+                .long("ex-suffix")
+                .help("Print the executable suffix")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("lib-extension")
+                .short('l')
+                .long("lib-extension")
+                .help("Print the library extension")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("lib-suffix")
+                .short('s')
+                .long("lib-suffix")
+                .help("Print the library suffix")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("lib-prefix")
+                .short('p')
+                .long("lib-prefix")
+                .help("Print the library prefix")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
+    match matches.get_flag("arch") {
+        true => println!("{}", ARCH),
+        false => {
+            // Pass
         }
     }
-}
-
-fn help() {
-    println!("-a OR --arch             : Print the CPU architecture");
-    println!("-o OR --operating-system : Print the operating system");
-    println!("-f OR --family           : Print the OS family");
-    println!("-ext OR --ex-extension   : Print the executable extension");
-    println!("-exs OR --ex-suffix      : Print the executable suffix");
-    println!("-libt OR --lib-extension : Print the library extension");
-    println!("-libs OR --lib-suffix    : Print the library suffix");
-    println!("-libp OR --lib-prefix    : Print the library prefix");
-    print!("\n");
-    println!("-h OR --help : Print this help text");
+    match matches.get_flag("os") {
+        true => println!("{}", OS),
+        false => {
+            // Pass
+        }
+    }
+    match matches.get_flag("family") {
+        true => println!("{}", FAMILY),
+        false => {
+            // Pass
+        }
+    }
+    match matches.get_flag("ex-extension") {
+        true => println!("{}", EXE_EXTENSION),
+        false => {
+            // Pass
+        }
+    }
+    match matches.get_flag("ex-suffix") {
+        true => println!("{}", EXE_SUFFIX),
+        false => {
+            // Pass
+        }
+    }
+    match matches.get_flag("lib-extension") {
+        true => println!("{}", DLL_EXTENSION),
+        false => {
+            // Pass
+        }
+    }
+    match matches.get_flag("lib-suffix") {
+        true => println!("{}", DLL_SUFFIX),
+        false => {
+            // Pass
+        }
+    }
+    match matches.get_flag("lib-prefix") {
+        true => println!("{}", DLL_PREFIX),
+        false => {
+            // Pass
+        }
+    }
 }
