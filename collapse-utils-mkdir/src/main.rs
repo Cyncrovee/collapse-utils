1ac7caec1cde83800ab67efdb499bd0cd8aae7e3
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-04-03 20:29:06 +0100

    refactor: clean up code in the "mkfile" & "mkdir" utils

--- collapse-utils-mkdir/src/main.rs
+++ collapse-utils-mkdir/src/main.rs
@@ -1,15 +1,8 @@
-use std::{
-    env::args,
-    fs::create_dir_all
-};
+use std::{env::args, fs::create_dir_all};
 
 fn main() {
     let args: Vec<_> = args().collect();
     for dir_arg in &args[1..args.iter().count()] {
-        let create_dir_result = create_dir_all(&dir_arg);
-        let _ = match create_dir_result {
-            Ok(dir) => dir,
-            Err(_) => panic!("Failed to create directory")
-        };
+        create_dir_all(&dir_arg).expect("Failed to create directory");
     }
 }
--- collapse-utils-mkfile/src/main.rs
+++ collapse-utils-mkfile/src/main.rs
@@ -1,15 +1,8 @@
-use std::{
-    env::args,
-    fs::File
-};
+use std::{env::args, fs::File};
 
 fn main() {
     let args: Vec<_> = args().collect();
     for file_arg in &args[1..args.iter().count()] {
-        let create_file_result = File::create(&file_arg);
-        let _ = match create_file_result {
-            Ok(file) => file,
-            Err(_) => panic!("Failed to create file")
-        };
+        File::create(&file_arg).expect("Failed to create file");
     }
 }
